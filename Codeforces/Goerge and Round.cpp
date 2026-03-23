bd427f5be6e2fe3cae87d1efa7dea2b855834eb1
PragyaUpadhya <pragyauadhyay1735@gmail.com>
2026-03-23 23:19:22 +0530

    uploading the "george and round" problem

--- Codeforces/Goerge and Round.cpp
+++ Codeforces/Goerge and Round.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 int main(){
     string t,s;
-    cin>>t;
-    cin>>s;
-    int sh=(s[0]-'0')*10+(s[1]-'0');//at '0' will help to multiply string with number snd print the asky value
-    int sm =(s[3]-'0')*10+(s[4]-'0');
-    int th = (s[0]-'0')*10+(s[1]-'0');
-    int tm = (s[3]-'0')*10+(s[4]-'0');
-   
+    cin >> t >> s;
 
-    int m = sh*60+sm;
-    int n = th*60+tm;
-    int x = m-n;
-    if(x<0){
-        x+= 1440;
-    }
-int a = x/60;
-int b = x/60;
-if(a<10) cout<<"0";
-cout<<a<<" ";
-if(b<10) cout<<"0";
-cout<<b;
+    int sh = (s[0]-'0')*10 + (s[1]-'0');
+    int sm = (s[3]-'0')*10 + (s[4]-'0');
+
+    int th = (t[0]-'0')*10 + (t[1]-'0');
+    int tm = (t[3]-'0')*10 + (t[4]-'0');
+
+    int m = sh*60 + sm;
+    int n = th*60 + tm;
+
+    int x = n - m;
+    if(x < 0) x += 1440;
+
+    int a = x / 60;
+    int b = x % 60;
+
+    if(a < 10) cout << "0";
+    cout << a << ":";
+    if(b < 10) cout << "0";
+    cout << b << "\n";
 }
